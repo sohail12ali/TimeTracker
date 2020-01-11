007359f8461ab86cb42efbe08c59214d87156162
Sohail Ali <sohail@outlook.in>
2020-01-11 16:42:48 +0530

    Add project files.

---
+++ TimeTracker/TimeTracker/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using TimeTracker.Services;
+using TimeTracker.Views;
+
+namespace TimeTracker
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
